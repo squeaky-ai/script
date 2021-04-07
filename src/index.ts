d1ca5e723306ca8c26eef029a7ef96c03ee78bd7
Lewis Monteith <lewismonteith@gmail.com>
2021-04-07 21:08:56 +0100

    Scrape the html

--- src/index.ts
+++ src/index.ts
@@ -20,93 +20,105 @@ interface State {
   href: string;
   userAgent: string;
   locale: string;
+  html: string;
 }
 
-const siteId = window._sqSettings.site_id;
-const viewer = window._sqSettings.viewier_id;
+(function main() {
+  window._sqSettings = window._sqSettings || {};
 
-if (!siteId || !viewer) {
-  console.warn('Squeaky has been incorrectly configured');
-}
+  const siteId = window._sqSettings.site_id;
+  const viewer = window._sqSettings.viewier_id;
 
-const socket = new WebSocket(`wss://gateway.squeaky.ai?site_id=${window._sqSettings.site_id}&viewer=${viewer}`);
-
-socket.addEventListener('open', (): void => {
-  const state: State = {
-    viewportX: window.innerWidth,
-    viewportY: window.innerHeight,
-    scrollX: window.scrollX,
-    scrollY: window.scrollY,
-    mouseX: 0,
-    mouseY: 0,
-    action: null,
-    href: location.href,
-    userAgent: navigator.userAgent,
-    locale: navigator.language
-  };
-
-  function registerAction(element: Element, action: string): void {
-    let selector = element.nodeName.toLowerCase();
-    if (element.id) selector += '#' + element.id;
-    if (element.className) selector += '.' + element.className;
-    state.action = action + ':' + selector;
+  if (!siteId || !viewer) {
+    console.warn('Squeaky has been incorrectly configured');
+    return;
   }
 
-  function ticker(): void {
-    socket.send(JSON.stringify({
-      action: 'event',
-      payload: state
-    }));
-    state.action = null;
-  }
+  const socket = new WebSocket(`wss://gateway.squeaky.ai?site_id=${window._sqSettings.site_id}&viewer=${viewer}`);
 
-  let watch = setInterval(ticker, 250);
+  socket.addEventListener('open', (): void => {
+    const state: State = {
+      viewportX: window.innerWidth,
+      viewportY: window.innerHeight,
+      scrollX: window.scrollX,
+      scrollY: window.scrollY,
+      mouseX: 0,
+      mouseY: 0,
+      action: null,
+      href: location.href,
+      userAgent: navigator.userAgent,
+      locale: navigator.language,
+      html: getDomContents(),
+    };
 
-  window.addEventListener('mousemove', (event: MouseEvent): void => {
-    state.mouseX = event.clientX;
-    state.mouseY = event.clientY; 
-  });
-
-  window.addEventListener('scroll', (_event: Event): void => {
-    state.scrollX = window.scrollX;
-    state.scrollY = window.scrollY;
-  });
-
-  window.addEventListener('click', (event: MouseEvent): void => {
-    const target = event.target as Element;
-    const action = 'click';
-    const exclusions = ['input'];
-
-    if (!exclusions.includes(target.nodeName.toLowerCase())) {
-      registerAction(target, action);
+    function registerAction(element: Element, action: string): void {
+      let selector = element.nodeName.toLowerCase();
+      if (element.id) selector += '#' + element.id;
+      if (element.className) selector += '.' + element.className;
+      state.action = action + ':' + selector;
     }
-  });
 
-  window.addEventListener('focusin', (event: FocusEvent): void => {
-    const action = 'focus';
-    const target = event.target as Element;
-    registerAction(target, action);
-  });
-  
-  window.addEventListener('focusout', (event: FocusEvent): void => {
-    const action = 'blur';
-    const target = event.target as Element;
-    registerAction(target, action);
-  });
+    function ticker(): void {
+      const message = JSON.stringify({
+        action: 'event',
+        payload: Object.assign({}, state, { html: getDomContents() })
+      });
 
-  window.addEventListener('focus', (): void => {
-    const action = 'focus';
-    const target = document.body;
-    registerAction(target, action);
-    clearInterval(watch);
-    watch = setInterval(ticker, 250);
-  });
+      socket.send(message);
+      state.action = null;
+    }
 
-  window.addEventListener('blur', (): void => {
-    const action = 'blur';
-    const target = document.body;
-    registerAction(target, action);
-    clearInterval(watch);
-    watch = undefined;
+    function getDomContents(): string {
+      return new XMLSerializer().serializeToString(document);
+    }
+
+    let watch = setInterval(ticker, 250);
+
+    window.addEventListener('mousemove', (event: MouseEvent): void => {
+      state.mouseX = event.clientX;
+      state.mouseY = event.clientY; 
+    });
+
+    window.addEventListener('scroll', (_event: Event): void => {
+      state.scrollX = window.scrollX;
+      state.scrollY = window.scrollY;
+    });
+
+    window.addEventListener('click', (event: MouseEvent): void => {
+      const target = event.target as Element;
+      const action = 'click';
+      const exclusions = ['input'];
+
+      if (!exclusions.includes(target.nodeName.toLowerCase())) {
+        registerAction(target, action);
+      }
+    });
+
+    window.addEventListener('focusin', (event: FocusEvent): void => {
+      const action = 'focus';
+      const target = event.target as Element;
+      registerAction(target, action);
+    });
+    
+    window.addEventListener('focusout', (event: FocusEvent): void => {
+      const action = 'blur';
+      const target = event.target as Element;
+      registerAction(target, action);
+    });
+
+    window.addEventListener('focus', (): void => {
+      const action = 'focus';
+      const target = document.body;
+      registerAction(target, action);
+      clearInterval(watch);
+      watch = setInterval(ticker, 250);
+    });
+
+    window.addEventListener('blur', (): void => {
+      const action = 'blur';
+      const target = document.body;
+      registerAction(target, action);
+      clearInterval(watch);
+    });
   });
-});
+})();
