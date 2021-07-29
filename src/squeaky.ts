5da081807de99b702ea30c6f56be13cf01b22981
Lewis Monteith <lewismonteith@gmail.com>
2021-07-29 10:36:25 +0100

    Fire fake page views for single page apps

--- src/squeaky.ts
+++ src/squeaky.ts
@@ -1,23 +1,29 @@
 import { io, Socket } from 'socket.io-client';
 import { record, EventType } from 'rrweb';
 
-export class Squeaky {
-  private socket: Socket;
+interface State {
+  previousPath: string;
+}
 
-  private blockedUserAgents = [
-    'ahrefsbot',
-    'desktop app',
-    'facebookexternalhit',
-    'webflow'
-  ];
+export class Squeaky {
+  private site_id: string;
+  private socket: Socket;
+  private stop: Function;
+  private state: State;
 
   public constructor(site_id: string) {
-    if (this.ignore) return;
+    this.site_id = site_id;
+    this.state = { previousPath: '' };
 
+    this.install();
+    this.watchForPageChange();
+  }
+
+  private install = () => {
     this.socket = io(WEBSOCKET_SERVER_HOST, {
       path: '/gateway/socket',
       query: {
-        site_id,
+        site_id: this.site_id,
         viewer_id: this.getOrCreateId('viewer', localStorage),
         session_id: this.getOrCreateId('session', sessionStorage),
       },
@@ -25,10 +31,37 @@ export class Squeaky {
     });
 
     this.socket.on('connect', this.onConnected);
-  }
+  };
+
+  private watchForPageChange = () => {
+    // Single page apps don't fire a page view and don't
+    // disconnect when they nagigate. This causes events
+    // to stack up into the hundreds of thousands which
+    // the database is not happy with. This fakes the 
+    // process and reconnects every page change.
+    const observer = new MutationObserver(() => {
+      if (location.pathname !== this.state.previousPath) {
+        this.reconnect();
+      }
+    });
+
+    // These might not all be necessary but it's cheap
+    observer.observe(document.body, {
+      characterDataOldValue: true, 
+      subtree: true, 
+      childList: true, 
+      characterData: true
+    });
+  };
+
+  private reconnect = (): void => {
+    this?.stop();
+    this.socket.disconnect();
+    this.install();
+  };
 
   private onConnected = (): void => {
-    record({
+    this.stop = record({
       maskAllInputs: true,
       slimDOMOptions: {
         script: true,
@@ -49,12 +82,6 @@ export class Squeaky {
       }
     });
   };
-
-  private get ignore(): boolean {
-    const match = this.blockedUserAgents
-      .find(ua => navigator.userAgent.toLowerCase().includes(ua));
-    return !!match;
-  }
 
   private getOrCreateId(type: 'session' | 'viewer', storage: Storage): string {
     const id = storage.getItem(`squeaky_${type}_id`) || Math.random().toString(36).slice(-8);
