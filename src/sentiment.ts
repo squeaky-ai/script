dd790163501c4326ab04005a3f382237f2812861
Lewis Monteith <lewismonteith@gmail.com>
2021-11-30 20:14:31 +0000

    Embed the smaller svgs and fix padding

--- src/sentiment.ts
+++ src/sentiment.ts
@@ -2,7 +2,6 @@
 import type { Visitor } from './visitor';
 import type { Feedback } from './types/feedback';
 
-const SMILY_FACE_IMAGE_URL = 'https://cdn.squeaky.ai/g/assets/smiley-face.svg';
 const SENTIMENT_BUTTON_CSS_URL = 'https://cdn.squeaky.ai/g/assets/sentiment-button.css';
 
 export class Sentiment {
@@ -26,7 +25,12 @@ export class Sentiment {
     button.id = 'squeaky__sentiment_open';
     button.classList.add(this.settings.sentiment_layout);
     button.style.background = this.settings.sentiment_accent_color;
-    button.innerHTML = `<img src='${SMILY_FACE_IMAGE_URL}' height='20' width='20' alt='Smiley face' /> Feedback`;
+    button.innerHTML = `
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='20' height='20'>
+        <path d='M12 22a10 10 0 1 1 0-20 10 10 0 1 1 0 20zm-5-9a5 5 0 0 0 5 5 5 5 0 0 0 5-5h-2a3 3 0 0 1-3 3 3 3 0 0 1-3-3H7zm1-2a1.5 1.5 0 0 0 1.5-1.5A1.5 1.5 0 0 0 8 8a1.5 1.5 0 0 0-1.5 1.5A1.5 1.5 0 0 0 8 11zm8 0a1.5 1.5 0 0 0 1.5-1.5A1.5 1.5 0 0 0 16 8a1.5 1.5 0 0 0-1.5 1.5A1.5 1.5 0 0 0 16 11z' fill='#fff' opacity='.65' />
+      </svg>
+      Feedback
+    `;
     button.addEventListener('click', this.handleSentimentOpen);
 
     return button;
@@ -65,7 +69,12 @@ export class Sentiment {
     const button = document.createElement('button');
 
     button.id = 'squeaky__sentiment_close';
-    button.innerText = 'x';
+    button.innerHTML = `
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='16' height='16'>
+        <path fill='none' d='M0 0h24v24H0z' />
+        <path fill='#ffffff' d='M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z'  />
+      </svg>
+    `;
     button.style.background = this.settings.sentiment_accent_color;
     button.addEventListener('click', this.handleSentimentClose);
 
