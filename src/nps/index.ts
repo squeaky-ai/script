6f4edcfe748bfe201b14d0afb84df63d9173b377
Lewis Monteith <lewismonteith@gmail.com>
2021-12-04 10:03:18 +0000

    Import the scripts

--- src/nps/index.ts
+++ src/nps/index.ts
@@ -1,3 +1,84 @@
 import './styles/main.scss';
 
-console.log('nps!');
+declare global {
+  interface Window {
+    showFollowUpQuestion: boolean;
+    showContactConsentQuestion: boolean;
+  }
+}
+
+class Nps {
+  private step: number = 0;
+
+  public onRatingInputChange = () => {
+    if (!window.showFollowUpQuestion) {
+      return this.submitForm();
+    }
+
+    this.setStep(1);
+  };
+
+  public onSubmitButtonClick = () => {
+    if (this.step === 1 && !window.showContactConsentQuestion) {
+      return this.submitForm();
+    }
+
+    if (this.step === 2 && this.form.contact.value === '0') {
+      return this.submitForm();
+    }
+
+    if (this.step === 3) {
+      return this.submitForm();
+    }
+
+    this.setStep(this.step + 1);
+  };
+
+  public onConfirmClose = () => {
+    const message = JSON.stringify({ 
+      key: '__squeaky_close_nps', 
+      value: {} 
+    });
+
+    window.parent.postMessage(message, '*');
+  };
+
+  public submitForm = () => {
+    const message = JSON.stringify({ 
+      key: '__squeaky_submit_nps', 
+      value: {}, 
+    });
+
+    window.parent.postMessage(message, '*');
+
+    document.body.style.display = 'none';
+
+    this.setStep(4);
+    this.form.submit();
+  };
+
+  public setStep = (step: number) => {
+    if (this.step >= step) return;
+
+    this.step = step;
+
+    const message = JSON.stringify({ 
+      key: '__squeaky_set_step_nps', 
+      value: { step }, 
+    });
+
+    window.parent.postMessage(message, '*');
+    this.wrapper.classList.add(`step-${step}`);
+  };
+
+  private get form(): HTMLFormElement {
+    return document.querySelector('.form')!;
+  }
+
+  private get wrapper(): HTMLDivElement {
+    return document.querySelector('.squeaky')!;
+  }
+}
+
+// Don't export an ES module or it won't work properly
+module.exports = new Nps();
--- src/sentiment/index.ts
+++ src/sentiment/index.ts
@@ -1,3 +1,20 @@
 import './styles/main.scss';
 
-console.log('sentiment!');
+class Sentiment {
+  public onRatingInputChange = () => {
+    const form = document.querySelector('.form');
+    form?.classList.add('add-comment');
+  };
+
+  public onConfirmClose = () => {
+    const message = JSON.stringify({ 
+      key: '__squeaky_close_sentiment', 
+      value: {} 
+    });
+
+    window.parent.postMessage(message, '*');
+  };
+}
+
+// Don't export an ES module or it won't work properly
+module.exports = new Sentiment();
