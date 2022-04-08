bb0719c5893b42213eb72759f7010cab9167004e
Lewis Monteith <lewismonteith@gmail.com>
2022-04-08 20:51:21 +0100

    Add some coverage to squeaky file

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
   coverageProvider: 'v8',
   globals: {
     SESSION_CUT_OFF_MS: 5000,
+    WEB_HOST: 'http://localhost',
+    API_SERVER_HOST: 'http://localhost',
+    WEBSOCKET_SERVER_HOST: 'ws://localhost',
   },
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
